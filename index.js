88b37d5e553de2f1321da30ac8f410ea891a78f5
AndreyD <ai.danichev@yandex.ru>
2023-01-16 14:04:55 +0300

    fix user-agent

--- index.js
+++ index.js
@@ -28,6 +28,12 @@ const gameParams = {
 	isNewBuilder: true
 }
 
+const config = {
+	headers: {
+		'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36'
+	}
+}
+
 const getGameNumbers = async () => {
 	await axios.get(twentyOneUrl,{
 		params: twentyOneParams
@@ -157,4 +163,3 @@ start(80);
 
 parseNumbers();
 parseGames();
-
